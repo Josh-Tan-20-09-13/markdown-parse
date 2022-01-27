a12baea919c42ee5f298e780821678058a919895
fallenferric <fallenferric@gmail.com>
2022-01-26 16:39:33 -0800

    Added test comments

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,10 +1,10 @@
-import static org.junit.Assert.*;
-import org.junit.*;
-
-public class MarkdownParseTest {
-        @Test
-            public void addition() {
-                        assertEquals(2, 1 + 1);
-                            }
+import static org.junit.Assert.*; // Import junit assert
+import org.junit.*; // import junit
+// newline
+public class MarkdownParseTest { //markdownparsetest class file
+    @Test // signify that below method is a test
+    public void addition() { // the test method
+        assertEquals(2, 1 + 1); // the condition that is fulfilled for successful test
+    }
 }
 
